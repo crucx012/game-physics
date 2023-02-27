5fb4221fb4442d300ae60420e35b4d55929d5849
Wayne Black <wayne.black@samaritanministries.org>
2023-02-27 13:03:07 -0600

    simple movement

--- src/App.js
+++ src/App.js
@@ -2,47 +2,65 @@ import React from 'react';
 import Player from './components/Player.jsx';
 import './App.css';
 
+let interval = {};
+
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      keys: [0, 0],
       x: 0,
       y: 0,
-      move: 10,
+      move: 5,
       pacManWalls: true
     }
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.move = this.move.bind(this);
   }
 
   handleKeyDown(event) {
-    let {x: newX,y: newY} = this.state;
+    let {keys: newKeys} = this.state;
     if (event.key === 'w' || event.key === "ArrowUp") {
-      newY -= this.state.move;
+      newKeys[1]--;
     } else if (event.key === 'a' || event.key === "ArrowLeft") {
-      newX -= this.state.move;
+      newKeys[0]--;
     } else if (event.key === 's' || event.key === "ArrowDown") {
-      newY += this.state.move;
+      newKeys[1]++;
     } else if (event.key === 'd' || event.key === "ArrowRight") {
-      newX += this.state.move;
+      newKeys[0]++;
     }
+    this.setState({
+      keys: newKeys
+    })
+  }
+
+  move() {
+    let {x: newX,y: newY, move, keys} = this.state;
+    newX += move * keys[0];
+    newY += move * keys[1];
     const maxWidth = window.innerWidth;
     const maxHeight = window.innerHeight;
-    newX = (newX + maxWidth) % maxWidth;
-    newY = (newY + maxHeight) % maxHeight;
+    if (this.state.pacManWalls) {
+      newX = (newX + maxWidth) % maxWidth;
+      newY = (newY + maxHeight) % maxHeight;
+    }
     this.setState({
       x: newX,
       y: newY
     })
-    console.log(event.key)
   }
 
   componentDidMount(){
     document.addEventListener("keydown", this.handleKeyDown);
+    interval = setInterval(() => {
+      this.move()
+    }, 100);
   }
 
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyDown);
+    clearInterval(interval);
   }
 
   render(){
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,18 +2,45 @@ import React from 'react';
 
 class Player extends React.Component {
   render(props) {
-    const height = Math.min(50,window.innerHeight - this.props.Y);
-    const width = Math.min(50,window.innerWidth - this.props.X);
-    console.log(width,height)
-    const style = {
-      'height': `${height}px`,
-      'width': `${width}px`,
-      'margin-top': `${this.props.Y}px`,
-      'margin-left': `${this.props.X}px`
-    }
+    let markers = this.getMarkers(props);
     return (
-      <div className="blue" style={style}></div>
+      <>
+        {markers}
+      </>
     );
+  }
+
+  getMarkers(props) {
+    let result = [];
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 2; j++){
+        const size = 50;
+        let top = this.props.Y;
+        if (i > 0) {
+          top -= window.innerHeight;
+        }
+        let left = this.props.X;
+        if (j > 0) {
+          left -= window.innerWidth;
+        }
+        const style = {
+          'height': `${size}px`,
+          'width': `${size}px`,
+          'marginTop': `${top}px`,
+          'marginLeft': `${left}px`,
+          'borderRadius': `50%`
+        }
+        if (this.isVisible(top,left,size)) {
+          result.push(<div key={`player${i}${j}`} className="blue" style={style}></div>)
+        }
+      }
+    }
+    return result;
+  }
+
+  isVisible(top,left,size) {
+    return ((top >= 0 && top <= window.innerHeight) || (top + size >= 0 && top + size <= window.innerHeight))
+      && ((left >= 0 && left <= window.innerWidth) || (left + size >= 0 && left + size <= window.innerWidth))
   }
 }
 
