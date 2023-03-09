6e67174af786277bf0f8de68a4e11f73c8b05134
Wayne Black <wayne.black@samaritanministries.org>
2023-03-09 09:35:24 -0600

    handle jumping when adjusting the zoom

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ class App extends React.Component {
       directions: [0, 0],
       x: 0,
       y: 0,
-      xOffset: (window.innerWidth / 2) - (50 / 2),
-      yOffset: (window.innerHeight / 2) - (50 / 2),
+      xOffset: window.innerWidth / 2,
+      yOffset: window.innerHeight / 2,
       move: 5,
+      freeze: false,
+      freezeUntil: new Date(),
       pacManWalls: true,
       panelVisible: false
     }
@@ -40,17 +42,44 @@ class App extends React.Component {
   }
 
   move() {
-    let {x: newX,y: newY, move, directions} = this.state;
+    let {xOffset, yOffset, freeze, freezeUntil} = this.state;
+    const newXOffset = window.innerWidth / 2;
+    const newYOffset = window.innerHeight / 2;
+    if (xOffset !== newXOffset || yOffset !== newYOffset) {
+      this.pause(newXOffset, newYOffset);
+      return;
+    } else if (freeze) {
+      if (new Date().getTime() >= freezeUntil.getTime()) {
+        this.setState({
+          freeze: false
+        })
+      } else {
+        return;
+      }
+    }
+    this.handleMove();
+  }
+
+  handleMove() {
+    let {x: newX, y: newY, move, directions} = this.state;
     newX += move * directions[0];
     newY -= move * directions[1];
-    const newXOffset = (window.innerWidth / 2) - (50 / 2);
-    const newYOffset = (window.innerHeight / 2) - (50 / 2);
     this.setState({
+      freeze: false,
       x: newX,
-      y: newY,
+      y: newY
+    })
+  }
+
+  pause(newXOffset, newYOffset) {
+    var date = new Date();
+    date.setMilliseconds(date.getMilliseconds() + 500);
+    this.setState({
+      freeze: true,
+      freezeUntil: date,
       xOffset: newXOffset,
       yOffset: newYOffset
-    })
+    });
   }
 
   togglePanel(isVisible) {
@@ -65,7 +94,6 @@ class App extends React.Component {
       this.move()
     }, 15);
   }
-
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyDown);
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class Player extends React.Component {
   render(props) {
     let markers = this.getMarkers(props);
+    if (this.props.state.freeze) return (<></>);
     return (
       <>
         {markers}
@@ -11,34 +12,45 @@ class Player extends React.Component {
   }
 
   getMarkers(props) {
-    let result = [];
     let {x,y,xOffset,yOffset,pacManWalls} = this.props.state;
-    x += xOffset;
-    y += yOffset;
+    let size = 50;
+    x += xOffset - (size / 2);
+    y += yOffset - (size / 2);
     if (pacManWalls) {
       x = ((x % window.innerWidth) + window.innerWidth) % window.innerWidth;
       y = ((y % window.innerHeight) + window.innerHeight) % window.innerHeight;
     }
+    return this.getClones(x,y);
+  }
+
+  getClones(x,y) {
+    let result = [];
     for (let i = 0; i < 2; i++) {
       for (let j = 0; j < 2; j++){
-        let top = y;
-        if (i > 0) {
-          top -= window.innerHeight;
-        }
-        let left = x;
-        if (j > 0) {
-          left -= window.innerWidth;
-        }
-        if (this.isVisible(top,left)) {
-          const style = {
-            'marginTop': `${top}px`,
-            'marginLeft': `${left}px`,
-          }
-          result.push(<div key={`player${i}${j}`} className="blue circle" style={style}></div>)
-        }
+        result.push(this.getMarker(x,y,i,j))
       }
     }
     return result;
+  }
+
+  getMarker(x,y,i,j) {
+    let top = y;
+    if (i > 0) {
+      top -= window.innerHeight;
+    }
+    let left = x;
+    if (j > 0) {
+      left -= window.innerWidth;
+    }
+    if (this.isVisible(top,left)) {
+      const style = {
+        'marginTop': `${top}px`,
+        'marginLeft': `${left}px`,
+      }
+      return (
+        <div key={`player${i}${j}`} className="blue circle" style={style}></div>
+      );
+    }
   }
 
   isVisible(top,left) {
